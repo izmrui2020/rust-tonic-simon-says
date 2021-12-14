3fa24d3c1ef449fcb00bfa36505c796d72c46614
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-12-14 23:07:51 +0900

    save 1214

--- server/rust-grpc-web-server/src/main.rs
+++ server/rust-grpc-web-server/src/main.rs
@@ -1,4 +1,21 @@
+use anyhow::{Context, Result};
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod simonsays {
+    tonic::include_proto!("simonsays");
+}
+
+#[derive(Debug, Default)]
+pub struct SimonSays {}
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50000".parse()?;
+    let simonsays = SimonSays::default();
+
+    Server::builder()
+        .add_service(SimonSays::new(simonsays))
+        .serve(addr)
+        .await?;
+    Ok(())
 }
