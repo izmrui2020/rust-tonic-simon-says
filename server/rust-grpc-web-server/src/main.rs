f380681441ee0cd39538db906ac9a93b411e40d2
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-12-15 22:11:33 +0900

    fix part of getting proto definition

--- server/rust-grpc-web-server/src/main.rs
+++ server/rust-grpc-web-server/src/main.rs
@@ -1,17 +1,32 @@
-use anyhow::{Context, Result};
+use simonsays::simon_says_server::{SimonSays, SimonSaysServer};
+use simonsays::{Color, SimonRequest, SimonResponse};
 use tonic::{transport::Server, Request, Response, Status};
-
 pub mod simonsays {
     tonic::include_proto!("simonsays");
 }
+//use async_trait::async_trait;
 
 #[derive(Debug, Default)]
-pub struct SimonSays {}
+pub struct MySimonSays {}
+
+#[tonic::async_trait]
+impl SimonSays for MySimonSays {
+    async fn game(
+        &self,
+        request: Request<SimonRequest>,
+    ) -> Result<Response<SimonResponse>, Status> {
+        println!("get: {:?}", request);
+
+        let replay = "OK".to_string();
+
+        Ok(Response::new(replay))
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50000".parse()?;
-    let simonsays = SimonSays::default();
+    let simonsays = MySimonSays::default();
 
     Server::builder()
         .add_service(SimonSays::new(simonsays))
